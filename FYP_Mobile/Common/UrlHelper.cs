f9515d2857c0182a556a320af4117831ebe91b7d
Jake H <jakehumphreys@outlook.com>
2021-03-21 15:21:00 +0000

    Updated connection IP

--- FYP_Mobile/Common/UrlHelper.cs
+++ FYP_Mobile/Common/UrlHelper.cs
@@ -7,7 +7,7 @@ namespace FYP_Mobile.Common
 {
     public static class UrlHelper
     {
-        public static string ApiUrl = "https://192.168.0.24:45455/api/";
+        public static string ApiUrl = "https://192.168.0.101:45455/api/";
         public static string LoginUrl = ApiUrl + "Account/";
         public static string LocationUrl = ApiUrl + "StoredLocation";
         public static string NoteUrl = ApiUrl + "Note";
